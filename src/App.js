4466205d2739669c99c414030b4f0bb711292682
Nate <nathan.quan@uconn.edu>
2023-10-10 17:01:26 -0400

    fetched data for constructor standings and redered it

--- src/App.js
+++ src/App.js
@@ -46,8 +46,7 @@ function Box() {
   )
 }
 
-
-// function used to create a row in standings table
+// function used to create a row in driver standings table
   // Takes input of driver
 function DriverStandingRow({ driver }) {
   // If data is not None i.e. data is retrieved
@@ -112,13 +111,74 @@ function DriverStandings() {
   )
 };
 
+// function used to create a row in driver standings table
+// Takes input of driver
+function ConstructorStandingRow({ constr }) {
+  // If data is not None i.e. data is retrieved
+  if (constr){
+    // Return a table row with position, name, constructor, points
+    return (
+    <tr key={constr.constructorId}>
+      <td>{constr.position}</td>
+      <td>{constr.constructorName}</td>
+      <td>{constr.points}</td>
+    </tr>
+  );
+}
+  // If no data, return a message
+  else {
+    return <div>Data Unavailable</div>
+  }
+};
+
 function ConstructorStandings() {
+  // calls useFetchData to get data
+  let input = useFetchData('http://127.0.0.1:8000', '/constructor-standings')
+
+  // Checks if request is still loading
+    // If still loading, show that on the webpage
+  if (input.isLoading) {
+    return (
+      <div className="box flex justify-center">
+        <div>Loading...</div>
+      </div>
+    )
+  }
+
+  // when data finally loads, load the box with the table
+  return (
+    <div className="box flex flex-col">
+      <div className="sticky top-0">
+        <h1 className="h1 underline h-1/6">Constructor Standings</h1>
+      </div>
+      <div>
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th>Place</th>
+              <th>Team</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              input.data.map((constr, index) => (
+                <ConstructorStandingRow key={index} constr={constr} />
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+};
+/*function ConstructorStandings() {
   return (
     <div className="box">
       <h1 className="h1">Constructor Standings</h1>
     </div>
   )
-}
+}*/
 
 function Countdown() {
   /*const fetchData = async () => {
