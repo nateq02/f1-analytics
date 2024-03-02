84a11f0613e59099fa79d2c41222df45f0672390
Nate <nathan.quan@uconn.edu>
2024-03-01 19:13:24 -0500

    "Reset Create-React-App b/c dependencies were causing errors"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import './index.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import routes from "./routes"
-import { Home } from "./pages/Home"
-import { Results } from "./pages/Results"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/results" element={<Results />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-export { App };
+export default App;
